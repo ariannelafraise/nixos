83e774c95b4649d764090f780a68fe45994c1209
Develya <github@shield.paintilya.dev>
2024-08-10 15:55:00 -0400

    2024-08-10 15:54:53

--- home-manager/bash/bash.nix
+++ home-manager/bash/bash.nix
@@ -6,8 +6,8 @@
     historySize = 30000;
     historyFileSize = 300000;
     shellAliases = {
-      rebuild = "sudo nixos-rebuild switch --flake /etc/nixos";
-      "update_system_flake" = "sudo nix flake update /etc/nixos";
+      rebuild = "/etc/nixos/rebuild.sh";
+      update-system-flake = "sudo nix flake update /etc/nixos";
       uwu = "neofetch";
       patate-mount = "sudo mount /dev/sda1 /mnt/PATATE";
       patate-umount = "sudo umount /dev/sda1";
