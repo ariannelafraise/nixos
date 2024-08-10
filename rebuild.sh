9bdc6d3af2a3f674dffaad97197b0d1cc52cb98d
Develya <github@shield.paintilya.dev>
2024-08-10 15:48:40 -0400

    2024-08-10 15:48:33

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+commitMessage="$(date +'%Y-%m-%d %H:%M:%S')"
 repoPath="/etc/nixos"
 
 sudo nixos-rebuild switch --flake $repoPath && \
 sudo git -C $repoPath add /etc/nixos/ && \
-git -C $repoPath commit -m "$(date)" && \
+git -C $repoPath commit -m "$commitMessage" && \
 sudo git -C $repoPath push origin main && \
 echo "Sucessfully rebuilt and pushed to Github"
