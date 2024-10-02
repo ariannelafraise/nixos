8d67ca0e78ce1748757ff778ef1b93824df534b0
Develya <github@shield.paintilya.dev>
2024-10-02 07:42:37 -0400

    2024-10-02 07:42:37

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 commitMessage="$(date +'%Y-%m-%d %H:%M:%S')"
 repoPath="/etc/nixos"
 
-sudo nixos-rebuild switch --flake $repoPath && \
+#sudo nixos-rebuild switch --flake $repoPath && \
 sudo git -C $repoPath add /etc/nixos/ && \
 git -C $repoPath commit -m "$commitMessage" && \
 #sudo git -C $repoPath push origin main && \
