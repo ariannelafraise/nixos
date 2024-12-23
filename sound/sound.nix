0cb3f04ca08824232672d07628c90ea656cf2ea4
Arianne <github@shield.paintilya.dev>
2024-12-23 18:46:03 -0500

    REBUILD - 2024-12-23 18:45:48

--- sound/sound.nix
+++ sound/sound.nix
@@ -2,14 +2,14 @@
 {
   #hardware.pulseaudio.enable = true;
   # OR
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+  #security.rtkit.enable = true;
+  #services.pipewire = {
+   # enable = true;
+    #alsa.enable = true;
+    #alsa.support32Bit = true;
+    #pulse.enable = true;
     #jack.enable = false;
-  };
+  #};
 
-  services.wireplumber.enable = true;
+  #services.wireplumber.enable = true;
 }
